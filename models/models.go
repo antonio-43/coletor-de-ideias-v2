bea4e0856db01823423ddfbc9dc34d2baca2b784
rocha-43 <63781650+rocha-43@users.noreply.github.com>
2020-09-05 20:43:39 +0100

    response
    now i can see the response

---
+++ models/models.go
@@ -0,0 +1,18 @@
+package model
+
+type Idea struct {
+	Title       string
+	Description string
+}
+
+type Resp struct {
+	Head string
+	Body string
+}
+
+/*
+CREATE TABLE ideia (
+    title TEXT,
+    description TEXT
+);
+*/
